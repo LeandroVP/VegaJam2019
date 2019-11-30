8f528989d5f7472891252e19312bcec5bdddb9be
torcu <pablotorcuato@gmail.com>
2019-11-30 12:03:34 +0100

    MovimientoScript

--- Assets/Scripts/Movimiento.cs
+++ Assets/Scripts/Movimiento.cs
@@ -4,20 +4,19 @@ using UnityEngine;
 
 public class Movimiento : MonoBehaviour
 {
-
+    Rigidbody2D rb;
+    public float ejeX;
+    public float speed = 10.5f;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
+        ejeX = Input.GetAxis("Horizontal");
+        transform.Translate(new Vector3(ejeX * Time.deltaTime * speed, 0, 0));
         
     }
     
-    private void OnCollisionEnter2D(Collision2D conQuienChoco)
-    {
-        print(conQuienChoco.transform.name);
-        Destroy(conQuienChoco.gameObject);
-    }
 }
