37ec725118cc0ed2a86dc1289ea709b62aac0cc5
torcu <pablotorcuato@gmail.com>
2019-11-30 17:25:49 +0100

    RandomList

---
+++ Assets/Scripts/RandomTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomTest : MonoBehaviour
+{
+    public int[] listaRandom;
+    public int cantidadDeTeclas;
+    void Start()
+    {
+        for(int i = 0; i<cantidadDeTeclas; i++)
+        {
+            listaRandom[i] = i;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
