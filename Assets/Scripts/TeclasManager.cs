8dd8555087f36bbc9e969c4ff973efa1855a4cde
torcu <pablotorcuato@gmail.com>
2019-11-30 21:20:00 +0100

    Animaciones Piedras

--- Assets/Scripts/TeclasManager.cs
+++ Assets/Scripts/TeclasManager.cs
@@ -8,12 +8,14 @@ public class TeclasManager : MonoBehaviour
     public GameObject[] listaTeclas;
     void Start()
     {
-        for(int i = 0; i<transform.childCount; i++)
+        print("Tengo " + transform.childCount + " hijos");
+        for (int i = 0; i<transform.childCount; i++)
         {
             listaTeclas[i] = transform.GetChild(i).gameObject;
             listaTeclas[i].name = randomBox.GetComponent<RandomTest>().listaRandom[i].ToString();
             listaTeclas[i].gameObject.GetComponent<Activador>().numero = randomBox.GetComponent<RandomTest>().listaRandom[i];
         }
+        
     }
 
     void Update()
