7a5c387d4ef8bd9d8ef322f3304cd76fe7befa4d
LeandroVP <LeandroVP@users.noreply.github.com>
2019-12-01 17:34:04 +0100

    Menu finished

--- Assets/Scripts/Transiciones.cs
+++ Assets/Scripts/Transiciones.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class Transiciones : MonoBehaviour
 {
     private Animator _transicionAnim;
+    public GameObject fader;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,12 +16,13 @@ public class Transiciones : MonoBehaviour
 
     public void LoadScene(string scene)
     {
+        fader.GetComponent<FadeDark>().Oscurecer();
         StartCoroutine(Transiciona(scene));
     }
 
     IEnumerator Transiciona(string scene){
-        _transicionAnim.SetTrigger("salida");
-        yield return new WaitForSeconds(1);
+        
+        yield return new WaitForSeconds(1.75f);
         SceneManager.LoadScene(scene);
     }
 
